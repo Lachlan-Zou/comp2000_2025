1181b4f29a6f0f8e8fff77c80e7c352bd65ef4a6
Lachlan-Zou <lachlan.zou@students.mq.edu.au>
2025-09-25 14:16:59 +1000

    New module, updated main and readme
    This commit is supposed to be a start for the assignment, although a lot of functionality has not yet been added because I want to test something with the git commit and push.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-import java.awt.Dimension;
-import java.awt.Graphics;
-
+import java.awt.*;
+import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 public class Main extends JFrame {
+
     public static void main(String[] args) throws Exception {
       Main window = new Main();
       window.run();
@@ -24,10 +25,32 @@ public class Main extends JFrame {
 
     private Main() {
       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      this.setLayout(new BorderLayout());
+
       Canvas canvas = new Canvas();
+      this.add(canvas, BorderLayout.CENTER);
       this.setContentPane(canvas);
+
+      JPanel controls = new JPanel();
+      JRadioButton dogButton = new JRadioButton("dog");
+      JRadioButton catButton = new JRadioButton("cat");
+      JRadioButton birdButton = new JRadioButton("bird");
+
+      ButtonGroup group = new ButtonGroup();
+      group.add(dogButton);
+      group.add(catButton);
+      group.add(birdButton);
+
+      controls.add(dogButton);
+      controls.add(catButton);
+      controls.add(birdButton);
+
+      this.add(controls, BorderLayout.WEST);
+      
       this.pack();
       this.setVisible(true);
+      
+
     }
 
     public void run() {
